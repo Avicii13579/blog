9fce2f75f51d9a10f8626b872ed64180d8e19092
Rowan <18699499130@163.com>
2025-06-29 16:10:54 +0800

    first

--- docs/.vuepress/.temp/pages/demo/82fx2ska/index.html.js
+++ docs/.vuepress/.temp/pages/demo/82fx2ska/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/jiuan/Desktop/my-blog/docs/.vuepress/.temp/pages/demo/82fx2ska/index.html.vue"
-const data = JSON.parse("{\"path\":\"/demo/82fx2ska/\",\"title\":\"foo\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"foo\",\"createTime\":\"2025/06/28 23:58:57\",\"permalink\":\"/demo/82fx2ska/\"},\"readingTime\":{\"minutes\":0.04,\"words\":11},\"git\":{},\"filePathRelative\":\"notes/demo/foo.md\",\"headers\":[]}")
+const data = JSON.parse("{\"path\":\"/demo/82fx2ska/\",\"title\":\"foo\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"foo\",\"createTime\":\"2025/06/28 23:58:57\",\"permalink\":\"/demo/82fx2ska/\"},\"readingTime\":{\"minutes\":0.04,\"words\":11},\"git\":{},\"filePathRelative\":\"notes/demo/foo.md\",\"headers\":[],\"categoryList\":[{\"id\":\"4358b5\",\"sort\":10001,\"name\":\"notes\"},{\"id\":\"c19f38\",\"sort\":10002,\"name\":\"demo\"}]}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
