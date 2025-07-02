ebcdb69e990e3359a6041a15be939ce6f8182be0
Rowan Lee <liyuanyuan@reach24h.com>
2025-07-02 17:29:27 +0800

    更新打包文件

--- docs/.vuepress/.temp/internal/articleTagColors.js
+++ docs/.vuepress/.temp/internal/articleTagColors.js
@@ -1,15 +1,2 @@
 import './articleTagColors.css'
-export const articleTagColors = {"生活感悟":"nckb","个人成长":"v895","随笔":"cgfd","jottings":"0j1i","预览":"nckb","组件":"hohf","markdown":"v895","CSS":"at2o","Grid":"y5ad","布局":"iiis","technology":"d9pd","VuePress":"gln9","教程":"ygl7","Vue3":"3u91","前端":"hohf","技巧":"2664","Giscus":"dhsk","投资":"v895","理财":"dhsk","财富管理":"v895","wealth":"tuy5","被动收入":"dhsk","财务自由":"4pn7"}
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateArticleTagColors) {
-    __VUE_HMR_RUNTIME__.updateArticleTagColors(articleTagColors)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ articleTagColors }) => {
-    __VUE_HMR_RUNTIME__.updateArticleTagColors(articleTagColors)
-  })
-}
+export const articleTagColors = {"生活感悟":"cu5l","个人成长":"73aq","随笔":"xa3r","jottings":"crly","预览":"cu5l","组件":"4mkm","markdown":"73aq","CSS":"omw7","Grid":"7gfn","布局":"1wp6","technology":"7dmr","VuePress":"coz2","教程":"b53v","Vue3":"sh9z","前端":"4mkm","技巧":"3hs3","Giscus":"9vxt","投资":"73aq","理财":"9vxt","财富管理":"73aq","wealth":"voea","被动收入":"9vxt","财务自由":"t1oe"}
